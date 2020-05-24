7e9ba0b8074209bc272a34f12836163ff623aacf
EvanLo427 <eggs.luo@gmail.com>
2020-05-23 23:38:01 -0600

    updated

--- lesson5/javascript/script.js
+++ lesson5/javascript/script.js
@@ -1,27 +1,63 @@
-let today = new Date();
-let date = today.getFullYear();
-document.getElementById('currentdate').innerHTML = date;
-
-let dateModified = document.lastModified;
-document.getElementById('dateandtime').innerHTML = dateModified
-
-let dayNames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-let d = new Date();
-let dayName = dayNames[d.getDay()];
-let monthName = months[d.getMonth()];
-let year = d.getFullYear();
-
-
-let fullDate = dayName + ", " + d.getDate() + " " + monthName + " " + year;
-document.getElementById("currentDate").innerHTML = fullDate;
-document.getElementById("year").textContent = year;
-
-const hambutton= document.querySelector(".ham");
-hambutton.addEventListener("click", toggleMenu, false);
-
-
 function toggleMenu() {
-    document.querySelector(".navigation").classList.toggle("responsive");
-}
+    let menu = document.getElementById('menu-list');
+    let menuToggle = document.getElementById('menu-toggle');
+  
+    if (!menu.classList.contains('show-menu')) {
+      menu.classList.add('show-menu');
+      menuToggle.innerHTML = 'Close';
+      menuToggle.classList.add('active-menu');
+    } else {
+      menu.classList.remove('show-menu');
+      menuToggle.innerHTML = '&#9776; Menu';
+      menuToggle.classList.remove('active-menu');
+    }
+  }
+  
+  let _currentDate = document.getElementById('current-date');
+  let _currentYear = document.getElementById('current-year');
+  
+  now = new Date();
+  
+  let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  
+  let months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  
+  let dayOfWeek = days[now.getDay()];
+  let month = months[now.getMonth()];
+  let day = now.getFullYear();
+  let year = now.getFullYear();
+  
+  _currentDate.innerHTML = `${dayOfWeek}, ${day} ${month} ${year}`;
+  _currentYear.innerHTML = now.getFullYear();
+  
+  let nav = 'preston';
+  
+  function changeNav(navID) {
+    document.getElementById(navID).classList.add('highlight');
+    if (nav !== navID) {
+      document.getElementById(nav).classList.remove('highlight');
+      nav = navID;
+    }
+  }
+  
+  changeNav('preston');
+  
+  let d = new Date();
+  
+  if (d.getDay() == 5) {
+    document.getElementById('banner').classList.add('show-banner');
+  }
+  
