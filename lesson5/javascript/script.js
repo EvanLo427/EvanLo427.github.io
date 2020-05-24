61ba483541e2f205e31961efdf929b3b5eef95b8
EvanLo427 <eggs.luo@gmail.com>
2020-05-23 23:55:17 -0600

    updated

--- lesson5/javascript/script.js
+++ lesson5/javascript/script.js
@@ -12,8 +12,7 @@ function toggleMenu() {
       menuToggle.classList.remove('active-menu');
     }
   }
-  
-  let dayNames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+let dayNames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 let d = new Date();
