be377554b44807a8fcbb7253ef147fca993a0f58
EvanLo427 <eggs.luo@gmail.com>
2020-06-01 22:49:41 -0600

    updated

---
+++ lesson7/javascript/lazyload.js
@@ -0,0 +1,8 @@
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+
