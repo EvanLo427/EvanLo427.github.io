3f68ff18a30013e1ef2096dd6fb97357aaf86565
EvanLo427 <eggs.luo@gmail.com>
2020-06-18 22:37:00 -0600

    updated

---
+++ lesson9/js/script.js
@@ -0,0 +1,32 @@
+//modifieddate
+let date = new Date();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+document.getElementById("currentDate").innerHTML = days[date.getDay()] + ", " + date.getDate() + " " + month[date.getMonth()] + " " + date.getFullYear();
+document.getElementById("year").textContent = new Date().getFullYear();
+
+//Toggle Menu
+const hambutton= document.querySelector(".ham");
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+    document.querySelector(".navigation").classList.toggle("responsive");
+}
+
+//Show Banner
+let d = new Date();
+let dayOfWeek = d.getDay();
+
+if (dayOfWeek == 5) {
+    document.getElementById("notice").style.display = 'block';
+} else {
+    document.getElementById("notice").style.display = 'none';
+}
+
+//form updateRange
+function updateRange(val) {
+    document.getElementById('stormSeverityVal').innerHTML = val;
+  }
+  
+  updateRange(1);
