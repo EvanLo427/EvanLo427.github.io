d610bde82d13e99f4666e6652af754269dee062a
EvanLo427 <eggs.luo@gmail.com>
2020-06-21 00:33:52 -0600

    updated

--- lesson9/js/script.js
+++ lesson9/js/script.js
@@ -23,52 +23,47 @@ if (dayOfWeek == 5) {
 } else {
     document.getElementById("notice").style.display = 'none';
 }
+
+
+//W09 Assignment JSON
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
-//
 fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-      console.table(jsonObject); // temporary checking for valid response and data parsing
-      const towns = jsonObject['towns'];
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        // console.table(jsonObject);  // temporary checking for valid response and data parsing
+        const towns = jsonObject['towns'];
+        const myTowns = towns.filter(town => (town.name == 'Preston' || town.name == 'Soda Springs' || town.name == 'Fish Haven'));
 
-      const southernTowns = towns.filter(town => (town.name == 'Preston' || town.name == 'Soda Springs' || town.name == 'Fish Haven'));
+        myTowns.forEach(town => {
 
-      southernTowns.forEach(town => {
+            let card = document.createElement('div');
+            let townData = document.createElement('div');
+            let townName = document.createElement('h2');
+            let townMotto = document.createElement('h3');
+            let townYear = document.createElement('p');
+            let townPopulation = document.createElement('p');
+            let townRain = document.createElement('p');
+            let image = document.createElement('img');
 
-        let eachTown = document.createElement('article');
-        let townContainer = document.createElement('div');
-        let townName = document.createElement('h2');
-        let townMotto = document.createElement('h3');
-        let yearFounded = document.createElement('p');
-        let population = document.createElement('p');
-        let annualRain = document.createElement('p');
-        let image = document.createElement('img');
+                townName.innerHTML = `${town.name}`;
+                townMotto.innerHTML = `${town.motto}`;
+                townYear.innerHTML = `Year Founded: ${town.yearFounded}`;
+                townPopulation.innerHTML = `Population: ${town.currentPopulation}`;
+                townRain.innerHTML = `Annual Rain Fall: ${town.averageRainfall}`;
 
-        eachTown.appendChild(townContainer);
-        eachTown.appendChild(image);
+                    card.appendChild(townData)
+                    townData.appendChild(townName);
+                    townData.appendChild(townMotto);
+                    townData.appendChild(townYear);
+                    townData.appendChild(townPopulation);
+                    townData.appendChild(townRain);
+                    image.setAttribute('src', `images/${town.photo}`);
+                    image.setAttribute('alt', `${town.name}: ${town.motto}`);
+                    card.appendChild(image);
 
-        townName.innerHTML = `${town.name}`;
-        townContainer.appendChild(townName);
-
-        townMotto.innerHTML = `${town.motto}`;
-        townContainer.appendChild(townMotto);
-
-        yearFounded.innerHTML = `Year Founded: ${town.yearFounded}`;
-        townContainer.appendChild(yearFounded);
-
-        population.innerHTML = `Population: ${town.currentPopulation}`;
-        townContainer.appendChild(population);
-
-        annualRain.innerHTML = `Annual Rain Fall: ${town.averageRainfall}`;
-        townContainer.appendChild(annualRain);
-
-        image.setAttribute('src', `images/${town.photo}`);
-        image.setAttribute('alt', `${town.name}: ${town.motto}`);
-        
-        document.querySelector('div.townContainer').appendChild(eachTown);
-
-      });
+            document.querySelector('div.townData').appendChild(card);
+        });
     });
