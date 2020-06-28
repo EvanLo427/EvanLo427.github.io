c3bf3d8f2e4e055321ab67ac6370b4ef4b637b33
EvanLo427 <eggs.luo@gmail.com>
2020-06-28 00:50:05 -0600

    updated

--- lesson10/js/script.js
+++ lesson10/js/script.js
@@ -25,45 +25,76 @@ if (dayOfWeek == 5) {
 }
 
 
-//W09 Assignment JSON
-const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+//W06 Weather API
+function windChill(t, s) {
+    if (t > 50 || s <= 3) {
+      return 'N/A';
+    }
+    const wc = Math.round(
+      35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16)
+    );
+  
+    return `${wc}&#176;F`;
+  }
 
-fetch(requestURL)
-    .then(function (response) {
+//W10 Weather API
+const requestURL ='https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=d240e58805731cb6287c5e4f95fa6d71';
+  
+    fetch(requestURL)
+    .then(response => {
+      if (response.ok) {
         return response.json();
+      } else {
+        throw new Error('Could not retrieve current weather!');
+      }
     })
-    .then(function (jsonObject) {
-        // console.table(jsonObject);  // temporary checking for valid response and data parsing
-        const towns = jsonObject['towns'];
-        const myTowns = towns.filter(town => (town.name == 'Preston' || town.name == 'Soda Springs' || town.name == 'Fish Haven'));
-
-        myTowns.forEach(town => {
-
-            let card = document.createElement('box');
-            let townData = document.createElement('div');
-            let townName = document.createElement('h2');
-            let townMotto = document.createElement('h3');
-            let townYear = document.createElement('p');
-            let townPopulation = document.createElement('p');
-            let townRain = document.createElement('p');
-            let image = document.createElement('img');
-
-                townName.innerHTML = `${town.name}`;
-                townMotto.innerHTML = `${town.motto}`;
-                townYear.innerHTML = `Year Founded: ${town.yearFounded}`;
-                townPopulation.innerHTML = `Population: ${town.currentPopulation}`;
-                townRain.innerHTML = `Annual Rain Fall: ${town.averageRainfall}`;
-
-                    card.appendChild(townData)
-                    townData.appendChild(townName);
-                    townData.appendChild(townMotto);
-                    townData.appendChild(townYear);
-                    townData.appendChild(townPopulation);
-                    townData.appendChild(townRain);
-                    image.setAttribute('src', `images/${town.photo}`);
-                    image.setAttribute('alt', `${town.name}: ${town.motto}`);
-                    card.appendChild(image);
-
-            document.querySelector('div.townData').appendChild(card);
-        });
+    .then(jsonObject => {
+      const resToday = jsonObject.list[0];
+  
+      const weatherToday = {
+        currently: resToday.weather[0].description,
+        temp: resToday.main.temp,
+        windSpeed: resToday.wind.speed,
+        humidity: resToday.main.humidity,
+      };
+  
+      document.getElementById('currently').innerHTML = weatherToday.currently
+        .toLowerCase()
+        .split(' ')
+        .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+        .join(' ');
+      document.getElementById('temp').innerHTML = Math.round(weatherToday.temp);
+      document.getElementById('humidity').innerHTML = weatherToday.humidity;
+      document.getElementById('windSpeed').innerHTML = weatherToday.windSpeed;
+      document.getElementById('windChill').innerHTML = windChill(
+        weatherToday.temp,
+        weatherToday.windSpeed
+      );
+  
+      for (i = 0; i < jsonObject.list.length; i++) {
+        let forecast = jsonObject.list[i];
+  
+        if (forecast.dt_txt.slice(-8) == '18:00:00') {
+          let td = document.createElement('td');
+          let img = document.createElement('img');
+          let temp = document.createTextNode(Math.round(forecast.main.temp));
+  
+          td.innerHTML = '&#176;';
+          td.prepend(temp);
+  
+          img.setAttribute(
+            'src',
+            `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`
+          );
+          img.setAttribute('alt', forecast.weather[0].main);
+  
+          td.prepend(img);
+  
+          document.getElementById('forecastRow').appendChild(td);
+        }
+      }
+    })
+    .catch(err => {
+      document.getElementById('weatherSummary').innerHTML = err;
+      document.getElementById('forecastRow').innerHTML = err;
     });
