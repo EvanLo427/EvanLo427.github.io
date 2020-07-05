2930191323c157c13e0a83e2d3d0c444946657a0
EvanLo427 <eggs.luo@gmail.com>
2020-07-04 23:11:30 -0600

    updated

---
+++ lesson11/js/script.js
@@ -0,0 +1,79 @@
+//modifieddate
+let date = new Date();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+document.getElementById("currentDate").innerHTML = days[date.getDay()] + ", " + date.getDate() + " " + month[date.getMonth()] + " " + date.getFullYear();
+document.getElementById("year").textContent = new Date().getFullYear();
+
+//Toggle Menu
+const hambutton= document.querySelector(".ham");
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+    document.querySelector(".navigation").classList.toggle("responsive");
+}
+
+//Show Banner
+let d = new Date();
+let dayOfWeek = d.getDay();
+
+if (dayOfWeek == 5) {
+    document.getElementById("notice").style.display = 'block';
+} else {
+    document.getElementById("notice").style.display = 'none';
+}
+
+//W06 Weather Summary
+function windChill(t, s) {
+    if (t > 50 || s <= 3) {
+      return 'N/A';
+    }
+    const wc = Math.round(
+      35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16)
+    );
+  
+    return `${wc}&#176;F`;
+  }
+
+//form updateRange
+function updateRange(val) {
+  document.getElementById('stormSeverityVal').innerHTML = val;
+}
+
+updateRange(1);
+
+//Lazyload
+const imgOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px 50px 0px"
+};
+
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+      image.removeAttribute('data-src');
+  };
+};
+
+if ('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if(item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} 
+
+else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
