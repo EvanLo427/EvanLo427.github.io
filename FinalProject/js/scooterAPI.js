0d3d257ed390b6b6e9f2d7d71cb2f59ec3838a92
EvanLo427 <eggs.luo@gmail.com>
2020-07-22 15:50:46 -0600

    updated

--- FinalProject/js/scooterAPI.js
+++ FinalProject/js/scooterAPI.js
@@ -10,7 +10,7 @@ fetch(requestURL)
         const scooters = jsonObject['scooters'];
         const myScooters = scooters.filter(scooter => (scooter.name == 'Honda Metro Scooter' || scooter.name == 'Honda Dio Scooter' || scooter.name == 'Honda PCX150 Scooter' || scooter.name == 'Honda Pioneer ATV' || scooter.name == 'Jeep Wrangler - 4 door with a/c' || scooter.name == 'Jeep Wrangler - 2 door'));
 
-        myScooters.forEach(town => {
+        myScooters.forEach(scooter => {
 
             let card = document.createElement('box');
             let scooterDataJson = document.createElement('div');
@@ -18,26 +18,26 @@ fetch(requestURL)
             let scooterMotto = document.createElement('h3');
             let scooterMaxPersons = document.createElement('p');
             let scooterRHalfDay = document.createElement('p');
-            let scooterRFullDay" = document.createElement('p');
+            let scooterRFullDay = document.createElement('p');
             let scooterWHalfDay = document.createElement('p');
-            let scooterWFullDay" = document.createElement('p');
+            let scooterWFullDay = document.createElement('p');
             let image = document.createElement('img');
 
                 scooterName.innerHTML = `${scooter.name}`;
                 scooterMotto.innerHTML = `${scooter.motto}`;
                 scooterMaxPersons.innerHTML = `Year Founded: ${scooter.yearFounded}`;
-                scooterRHalfDay.innerHTML = `Population: ${scooter.currentPopulation}`;
-                scooterRFullfDay.innerHTML = `Population: ${scooter.currentPopulation}`;
-                scooterWHalfDay.innerHTML = `Population: ${scooter.currentPopulation}`;
-                scooterWFullfDay.innerHTML = `Population: ${scooter.currentPopulation}`;
+                scooterRHalfDay.innerHTML = `Reservation Half Day(3 hrs) : $ ${scooter.currentPopulation}`;
+                scooterRFullDay.innerHTML = `Reservation Full Day: $ ${scooter.currentPopulation}`;
+                scooterWHalfDay.innerHTML = `Walk-in Half Day(3 hrs) : $ ${scooter.currentPopulation}`;
+                scooterWFullDay.innerHTML = `Walk-in Full Day: $ ${scooter.currentPopulation}`;
 
                     card.appendChild(scooterDataJson)
                     scooterDataJson.appendChild(scooterName);
                     scooterDataJson.appendChild(scooterMaxPersons);
                     scooterDataJson.appendChild(scooterRHalfDay);
-                    scooterDataJson.appendChild(scooterRFullfDay);
+                    scooterDataJson.appendChild(scooterRFullDay);
                     scooterDataJson.appendChild(scooterRHalfDay);
-                    scooterDataJson.appendChild(scooterWFullfDay);
+                    scooterDataJson.appendChild(scooterFullDay);
                     image.setAttribute('src', `images/${scooter.photo}`);
                     image.setAttribute('alt', `${scooter.name}: ${scooter.motto}`);
                     card.appendChild(image);
