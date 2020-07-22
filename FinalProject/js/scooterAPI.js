6943c91a24a861aeea2caed885bba81bec26d02a
EvanLo427 <eggs.luo@gmail.com>
2020-07-22 15:44:43 -0600

    updated

--- FinalProject/js/scooterAPI.js
+++ FinalProject/js/scooterAPI.js
@@ -6,35 +6,40 @@ fetch(requestURL)
         return response.json();
     })
     .then(function (jsonObject) {
-        // console.table(jsonObject);  // temporary checking for valid response and data parsing
+        //console.table(jsonObject);  // temporary checking for valid response and data parsing
         const scooters = jsonObject['scooters'];
-        const myscooters = scooters.filter(scooter => (scooter.name == 'Preston' || scooter.name == 'Soda Springs' || scooter.name == 'Fish Haven'));
+        const myScooters = scooters.filter(scooter => (scooter.name == 'Honda Metro Scooter' || scooter.name == 'Honda Dio Scooter' || scooter.name == 'Honda PCX150 Scooter' || scooter.name == 'Honda Pioneer ATV' || scooter.name == 'Jeep Wrangler - 4 door with a/c' || scooter.name == 'Jeep Wrangler - 2 door'));
 
-        myscooter.forEach(town => {
+        myScooters.forEach(town => {
 
             let card = document.createElement('box');
             let scooterDataJson = document.createElement('div');
             let scooterName = document.createElement('h2');
             let scooterMotto = document.createElement('h3');
-            let scooterYear = document.createElement('p');
-            let scooterPopulation = document.createElement('p');
-            let scooterRain = document.createElement('p');
+            let scooterMaxPersons = document.createElement('p');
+            let scooterRHalfDay = document.createElement('p');
+            let scooterRFullDay" = document.createElement('p');
+            let scooterWHalfDay = document.createElement('p');
+            let scooterWFullDay" = document.createElement('p');
             let image = document.createElement('img');
 
-                townName.innerHTML = `${town.name}`;
-                townMotto.innerHTML = `${town.motto}`;
-                townYear.innerHTML = `Year Founded: ${town.yearFounded}`;
-                townPopulation.innerHTML = `Population: ${town.currentPopulation}`;
-                townRain.innerHTML = `Annual Rain Fall: ${town.averageRainfall}`;
+                scooterName.innerHTML = `${scooter.name}`;
+                scooterMotto.innerHTML = `${scooter.motto}`;
+                scooterMaxPersons.innerHTML = `Year Founded: ${scooter.yearFounded}`;
+                scooterRHalfDay.innerHTML = `Population: ${scooter.currentPopulation}`;
+                scooterRFullfDay.innerHTML = `Population: ${scooter.currentPopulation}`;
+                scooterWHalfDay.innerHTML = `Population: ${scooter.currentPopulation}`;
+                scooterWFullfDay.innerHTML = `Population: ${scooter.currentPopulation}`;
 
                     card.appendChild(scooterDataJson)
-                    scooterDataJson.appendChild(townName);
-                    scooterDataJson.appendChild(townMotto);
-                    scooterDataJson.appendChild(townYear);
-                    scooterDataJson.appendChild(townPopulation);
-                    scooterDataJson.appendChild(townRain);
-                    image.setAttribute('src', `images/${town.photo}`);
-                    image.setAttribute('alt', `${town.name}: ${town.motto}`);
+                    scooterDataJson.appendChild(scooterName);
+                    scooterDataJson.appendChild(scooterMaxPersons);
+                    scooterDataJson.appendChild(scooterRHalfDay);
+                    scooterDataJson.appendChild(scooterRFullfDay);
+                    scooterDataJson.appendChild(scooterRHalfDay);
+                    scooterDataJson.appendChild(scooterWFullfDay);
+                    image.setAttribute('src', `images/${scooter.photo}`);
+                    image.setAttribute('alt', `${scooter.name}: ${scooter.motto}`);
                     card.appendChild(image);
 
             document.querySelector('div.scooterDataJson').appendChild(card);
