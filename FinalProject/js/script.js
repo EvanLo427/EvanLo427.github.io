1f48da5c67c41b59c7b0bc2c51d159fc26cef68d
EvanLo427 <eggs.luo@gmail.com>
2020-07-23 01:45:39 -0600

    updated

--- FinalProject/js/script.js
+++ FinalProject/js/script.js
@@ -1,45 +1,3 @@
-//modifieddate
-let date = new Date();
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-document.getElementById("currentDate").innerHTML = days[date.getDay()] + ", " + date.getDate() + " " + month[date.getMonth()] + " " + date.getFullYear();
-document.getElementById("year").textContent = new Date().getFullYear();
-
-//Toggle Menu
-const hambutton= document.querySelector(".ham");
-hambutton.addEventListener("click", toggleMenu, false);
-
-function toggleMenu() {
-    document.querySelector(".navigation").classList.toggle("responsive");
-}
-
-//Show Banner
-let d = new Date();
-let dayOfWeek = d.getDay();
-
-
-
-if (dayOfWeek >= 5) {
-    document.getElementById("banner-1").style.display = 'block';
-    document.getElementById("banner-2").style.display = 'none';
-} else{
-    document.getElementById("banner-1").style.display = 'none';
-    document.getElementById("banner-2").style.display = 'block';
-} 
-
-//W06 Weather Summary
-function windChill(t, s) {
-    if (t > 50 || s <= 3) {
-      return 'N/A';
-    }
-    const wc = Math.round(
-      35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16)
-    );
-  
-    return `${wc}&#176;F`;
-  }
-
 //Lazyload
 const imgOptions = {
   threshold: 0,
@@ -74,3 +32,43 @@ else {
     loadImages(img);
   });
 }
+
+//Toggle Menu
+const hambutton = document.querySelector(".ham");
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+    document.querySelector(".navigation").classList.toggle("responsive");
+}
+
+//Show Banner
+let d = new Date();
+let dayOfWeek = d.getDay();
+
+if (dayOfWeek >= 5) {
+    document.getElementById("banner-1").style.display = 'block';
+    document.getElementById("banner-2").style.display = 'none';
+} else{
+    document.getElementById("banner-1").style.display = 'none';
+    document.getElementById("banner-2").style.display = 'block';
+} 
+
+//W06 Weather Summary
+function windChill(t, s) {
+    if (t > 50 || s <= 3) {
+      return 'N/A';
+    }
+    const wc = Math.round(
+      35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16)
+    );
+  
+    return `${wc}&#176;F`;
+  }
+
+//modifieddate
+let date = new Date();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+document.getElementById("currentDate").innerHTML = days[date.getDay()] + ", " + date.getDate() + " " + month[date.getMonth()] + " " + date.getFullYear();
+document.getElementById("year").textContent = new Date().getFullYear();
