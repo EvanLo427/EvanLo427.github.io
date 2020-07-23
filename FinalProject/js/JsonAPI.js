b7d02949de17e708c2be33a3782a00e6f54591aa
EvanLo427 <eggs.luo@gmail.com>
2020-07-22 19:17:17 -0600

    updated

--- FinalProject/js/JsonAPI.js
+++ FinalProject/js/JsonAPI.js
@@ -1,5 +1,5 @@
 //W09 Assignment JSON
-const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+const requestURL = '/FinalProject/data/towndata.json';
 
 fetch(requestURL)
     .then(function (response) {
@@ -8,7 +8,7 @@ fetch(requestURL)
     .then(function (jsonObject) {
         // console.table(jsonObject);  // temporary checking for valid response and data parsing
         const towns = jsonObject['towns'];
-        const myTowns = towns.filter(town => (town.name == 'Preston' || town.name == 'Soda Springs' || town.name == 'Fish Haven'));
+        const myTowns = towns.filter(town => (town.name == 'Honda Metro Scooter' || town.name == 'Honda Dio Scooter' || town.name == 'Honda PCX150 Scooter'|| town.name == 'Honda Pioneer ATV'|| town.name == 'Jeep Wrangler - 4 door'|| town.name == 'Jeep Wrangler - 2 door'));
 
         myTowns.forEach(town => {
 
@@ -19,13 +19,15 @@ fetch(requestURL)
             let townYear = document.createElement('p');
             let townPopulation = document.createElement('p');
             let townRain = document.createElement('p');
+            let townBain = document.createElement('p');
             let image = document.createElement('img');
 
                 townName.innerHTML = `${town.name}`;
                 townMotto.innerHTML = `${town.motto}`;
-                townYear.innerHTML = `Year Founded: ${town.yearFounded}`;
-                townPopulation.innerHTML = `Population: ${town.currentPopulation}`;
-                townRain.innerHTML = `Annual Rain Fall: ${town.averageRainfall}`;
+                townYear.innerHTML = `Reservation Half Day(3hrs): $ ${town.reservationHalf}`;
+                townPopulation.innerHTML = `Reservation Full Day: $ ${town.reservationFull}`;
+                townRain.innerHTML = `Walk-In Half Day(3hrs): $ ${town.walkinHalf}`;
+                townBain.innerHTML = `Walk-In Full Day: $ ${town.walkinFull}`;
 
                     card.appendChild(townDataJson)
                     townDataJson.appendChild(townName);
@@ -33,6 +35,7 @@ fetch(requestURL)
                     townDataJson.appendChild(townYear);
                     townDataJson.appendChild(townPopulation);
                     townDataJson.appendChild(townRain);
+                    townDataJson.appendChild(townBain);
                     image.setAttribute('src', `images/${town.photo}`);
                     image.setAttribute('alt', `${town.name}: ${town.motto}`);
                     card.appendChild(image);
